c2438e80eb87f0d5fbe069da685b3417f48b7e85
Alex LaFroscia <alex@lafroscia.com>
2018-12-14 13:29:13 -0800

    chore(ci): remove testing scenarios around jQuery

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,30 +7,22 @@ module.exports = function() {
     getChannelURL('release'),
     getChannelURL('beta'),
     getChannelURL('canary')
-  ]).then((urls) => {
+  ]).then(urls => {
     return {
       useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-2.16',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.16.0'
             }
           }
         },
         {
           name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0'
             }
           }
@@ -56,19 +48,6 @@ module.exports = function() {
           npm: {
             devDependencies: {
               'ember-source': urls[2]
-            }
-          }
-        },
-        {
-          name: 'ember-default-with-jquery',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
-            })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1'
             }
           }
         }
