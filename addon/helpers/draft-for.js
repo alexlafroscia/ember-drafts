192f479186e3c1ccaed32eed9b8a500037b93249
Alex LaFroscia <alex@lafroscia.com>
2018-12-14 14:43:23 -0800

    fix(draft-for): unsubscribe from events on destroy

--- addon/helpers/draft-for.js
+++ addon/helpers/draft-for.js
@@ -4,13 +4,21 @@ import { service } from '@ember-decorators/service';
 export default class DraftFor extends Helper {
   @service drafts;
 
+  subscriber;
+
   compute([object], { resolver }) {
-    const subscriber = this.drafts.subscribe(object, resolver, () => {
-      subscriber.unsubscribe();
+    this.subscriber = this.drafts.subscribe(object, resolver, () => {
+      this.subscriber.unsubscribe();
 
       this.recompute();
     });
 
     return this.drafts.for(object, resolver);
+  }
+
+  willDestroy() {
+    if (this.subscriber) {
+      this.subscriber.unsubscribe();
+    }
   }
 }
