12144f0f8d0b21938087df5927d53c52c7b3f137
Alex LaFroscia <alex@lafroscia.com>
2018-12-17 15:37:44 -0800

    chore(ci): disable running CI against Ember < 3

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,22 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
           name: 'ember-release',
           npm: {
             devDependencies: {
